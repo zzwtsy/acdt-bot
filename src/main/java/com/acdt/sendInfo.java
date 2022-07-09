638367e6a6b403d5ae2b7976fe3eeb9a4eb9e8a2
zzwtsy <m3226257319@gmail.com>
2022-07-10 03:06:46 +0800

    添加：根据指令回复群消息

--- src/main/java/com/acdt/sendInfo.java
+++ src/main/java/com/acdt/sendInfo.java
@@ -4,13 +4,23 @@ import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.ListeningStatus;
 import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
+import net.mamoe.mirai.event.events.GroupMessageEvent;
 
 public class sendInfo extends SimpleListenerHost {
+    String message;
     @EventHandler
     private ListeningStatus onEvent(FriendMessageEvent event) {
-        String s = event.getMessage().contentToString();
-        if (s.equals("/电费")) {
+        message = event.getMessage().contentToString();
+        if (message.equals("#电费")) {
             event.getSender().sendMessage("功能还没写");
+        }
+        return ListeningStatus.LISTENING;
+    }
+    @EventHandler
+    private ListeningStatus onEvent(GroupMessageEvent event) {
+        message = event.getMessage().contentToString();
+        if (message.equals("#电费")) {
+            event.getGroup().sendMessage("功能还没写");
         }
         return ListeningStatus.LISTENING;
     }
