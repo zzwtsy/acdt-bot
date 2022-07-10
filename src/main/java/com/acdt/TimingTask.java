7dd40f510711c790c15e32e80d83f6a11ed7b6c1
zzwtsy <m3226257319@gmail.com>
2022-07-10 17:13:49 +0800

    定时检测剩余电量

---
+++ src/main/java/com/acdt/TimingTask.java
@@ -0,0 +1,25 @@
+package com.acdt;
+
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.event.SimpleListenerHost;
+
+import java.util.Objects;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class TimingTask extends SimpleListenerHost {
+
+    public void sendMessage() {
+        ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(2);
+        Runnable runnable = () -> {
+            GetInfo getInfo = new GetInfo();
+            getInfo.getInfo();
+            float electricityInformation = Float.parseFloat(getInfo.getElectricityPurchase()) + Float.parseFloat(getInfo.getSubsidy());
+            if (electricityInformation <= 20) {
+                Objects.requireNonNull(Bot.getInstances().get(0).getGroup(115604137)).sendMessage("电量不足20度，请及时充值");
+            }
+        };
+        scheduledExecutorService.scheduleAtFixedRate(runnable, 3, 3, TimeUnit.HOURS);
+    }
+}
