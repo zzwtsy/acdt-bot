df488f37948e81132d2db123ead889ef65717951
zzwtsy <m3226257319@gmail.com>
2022-07-13 03:29:10 +0800

    完善配置文件

--- src/main/java/com/acdt/acdtBotSettings.java
+++ src/main/java/com/acdt/acdtBotSettings.java
@@ -2,11 +2,46 @@ package com.acdt;
 
 public class acdtBotSettings {
     public static final acdtBotSettings INSTANCE = new acdtBotSettings();
-    int groupId;
-    int botId;
+    //qq群号
+    long groupId;
+    //机器人qq号
+    long botId;
+    //检测电费的时间间隔（小时）
     int taskTime;
-    String prefix;
+    //电费预警值
+    int threshold;
     String cookie;
+    //获取信息的指令
+    String infoCommand;
+    //获取帮助的指令
+    String helpCommand;
+
+    private acdtBotSettings() {
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public String getInfoCommand() {
+        return infoCommand;
+    }
+
+    public void setInfoCommand(String infoCommand) {
+        this.infoCommand = infoCommand;
+    }
+
+    public String getHelpCommand() {
+        return helpCommand;
+    }
+
+    public void setHelpCommand(String helpCommand) {
+        this.helpCommand = helpCommand;
+    }
 
     public int getTaskTime() {
         return taskTime;
@@ -24,29 +59,20 @@ public class acdtBotSettings {
         this.cookie = cookie;
     }
 
-    private acdtBotSettings() {}
-
-    public int getGroupId() {
+    public long getGroupId() {
         return groupId;
     }
 
-    public void setGroupId(int groupId) {
+    public void setGroupId(long groupId) {
         this.groupId = groupId;
     }
 
-    public int getBotId() {
+    public long getBotId() {
         return botId;
     }
 
-    public void setBotId(int botId) {
+    public void setBotId(long botId) {
         this.botId = botId;
     }
 
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
-    }
 }
