8ed36cace1b1e218f00e6ef4181ad553253638a2
zzwtsy <m3226257319@gmail.com>
2022-07-13 03:51:50 +0800

    删除不需要的依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,6 @@ repositories {
 }
 
 dependencies{
-    // https://mvnrepository.com/artifact/org.apache.httpcomponents/httpclient
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
     // https://mvnrepository.com/artifact/org.jsoup/jsoup
     implementation("org.jsoup:jsoup:1.15.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
